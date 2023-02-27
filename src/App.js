2dc693f9231f8cb1e64d15ba525af7f445ee6047
Bachir Boulaich <bachir.boulaich@outlook.com>
2023-02-27 14:15:29 +0100

    Error

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Accueil from "./components/Accueil";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
 import Liste from "./components/Liste";
+import Error from "./components/Error";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path='/' element={<Accueil/>}/>
         <Route path='/about' element={<About/>}/>
         <Route path='/liste' element={<Liste/>}/>
+        <Route path='/*' element={<Error/>}/>
       </Routes>
     </div>
   );
---
+++ src/components/Error.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error = () => {
+  return (
+    <div className='text-center'>
+        <span className='inline-block bg-red-600 text-white text-2xl rounded-lg border-2 border-red-700 px-5 py-3'>C'est pas la bonne route.</span>
+    </div>
+  )
+}
+
+export default Error
