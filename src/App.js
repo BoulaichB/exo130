a8b0c278c36a1e1f8ee16d38ad8f4ac43be19460
Bachir Boulaich <bachir.boulaich@outlook.com>
2023-02-27 13:58:53 +0100

    About

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Accueil from "./components/Accueil";
+import About from "./components/About";
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path='/' element={<Accueil/>}/>
+        <Route path='/about' element={<About/>}/>
       </Routes>
     </div>
   );
